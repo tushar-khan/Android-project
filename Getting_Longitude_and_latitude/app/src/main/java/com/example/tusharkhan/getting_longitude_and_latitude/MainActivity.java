5d666d7042d916c81c634e65b6074c96bdf8eb75
Tushar khan <tusherkhan573@gmail.com>
2018-08-12 23:15:11 +0600

    Getting longitude & Latitude

---
+++ Getting_Longitude_and_latitude/app/src/main/java/com/example/tusharkhan/getting_longitude_and_latitude/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.tusharkhan.getting_longitude_and_latitude;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    TextView tv;
+    LocationManager lm;
+    Location l;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.textview);
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        l = lm.getLastKnownLocation(lm.NETWORK_PROVIDER);
+        onLocationChanged(l);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double longitude=location.getLongitude();
+        double latitude=location.getLatitude();
+
+        tv.setText("Longitude: "+ longitude +"\n"+ "Latitude: "+ latitude );
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}

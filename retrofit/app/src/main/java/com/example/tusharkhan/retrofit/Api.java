e3e08de408c7c8823dcfc5e1bee59afd3decbb42
Tushar khan <tusherkhan573@gmail.com>
2018-08-15 14:15:59 +0600

    upadated retrofit

--- retrofit/app/src/main/java/com/example/tusharkhan/retrofit/Api.java
+++ retrofit/app/src/main/java/com/example/tusharkhan/retrofit/Api.java
@@ -1,11 +1,15 @@
 package com.example.tusharkhan.retrofit;
+import android.widget.EditText;
+
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface Api {
-    String BASE_URL = "https://api.github.com/users/";
-    @GET("tushar-khan")
-    Call<hero>getHeroes();
+    String BASE_URL = "https://api.github.com/";
+    @GET("users/{UserId}")
+    Call<hero>getHeroes(@Path("UserId") String login);
 }
--- retrofit/app/src/main/java/com/example/tusharkhan/retrofit/MainActivity.java
+++ retrofit/app/src/main/java/com/example/tusharkhan/retrofit/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.tusharkhan.retrofit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -16,36 +20,56 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-
+   // ListView listView;
+    TextView tv;
+    Button btn;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    //   listView = findViewById(R.id.listView);
+       et=findViewById(R.id.editText1);
+       btn=findViewById(R.id.getinfo);
+       tv=findViewById(R.id.text);
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               String s=et.getText().toString();
+               getHeroes(s);
+           }
+       });
 
-        final ListView listView = findViewById(R.id.listView);
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(Api.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
 
-        Api api = retrofit.create(Api.class);
-        Call<hero> call = api.getHeroes();
+
+    }
+    public void getHeroes(String s)
+    {
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl(Api.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+//String s="tushar-khan";
+    Api api = retrofit.create(Api.class);
+    Call<hero> call = api.getHeroes(s);
 
         call.enqueue(new Callback<hero>() {
-            @Override
-            public void onResponse(Call<hero> call, Response<hero> response) {
-                hero heroes = response.body();
-                String[] s = new String[2];
-                s[0] = heroes.getId().toString();
-                s[1]= heroes.getPublicRepos().toString();
-                listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, s));
-            }
+        @Override
+        public void onResponse(Call<hero> call, Response<hero> response) {
+            hero heroes = response.body();
+            tv.setText(heroes.getLogin());
+            tv.setText(heroes.getId().toString());
+            tv.setText(heroes.getPublicRepos().toString());
+         //  String[] s1 = new String[2];
+         //  s1[0] = heroes.getId().toString();
+         //  s[1]= heroes.getPublicRepos().toString();
+          //  listView.setAdapter(new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, s1));
+        }
 
-            @Override
-            public void onFailure(Call<hero> call, Throwable t) {
+        @Override
+        public void onFailure(Call<hero> call, Throwable t) {
 
-            }
-        });
-
+        }
+    });
     }
 }
